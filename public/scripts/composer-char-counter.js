35e5a0e95d24af777e830ee2686c26f1c1629a7d
shivangna <shivangna.kaistha@gmail.com>
2019-04-16 18:08:42 +0000

    composer-char-counter references correct bind event handler

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     console.log('validated!');
+      $("textarea[name='tweetbox']").keypress(function(){
+        console.log("textarea[name='tweetbox']")
+  });
 });
+
+
