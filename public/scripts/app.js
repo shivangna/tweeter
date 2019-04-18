2a004897cb2234c36d4b27501e35517e6481c033
shivangna <shivangna.kaistha@gmail.com>
2019-04-18 15:13:03 +0000

    dynamic tweets now appear

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,93 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+
+
+$(document).ready(function() {
+
+
+
+function createTweetElement(tweets) {
+
+const usernameElement = tweets.user.name
+const handleElement = tweets.user.handle
+const contentElement = tweets.content.text
+const profilePicElement = tweets.user.avatars.small
+const created_at = tweets.created_at
+
+
+var $addProfilePic = $('<img/>').addClass('profile-pic').attr("src", profilePicElement);
+var $addUsername = $("<h2>").addClass("username").text(usernameElement);
+var $addHandle = $("<h2>").addClass("tweeter-handle").text(handleElement);
+var $addContent = $("<div>").addClass("tweet-body").text(contentElement);
+var $addTime = $("<time>").text(handleElement);
+
+
+  let $result = $("<article>")
+  .addClass("posted-tweet")
+  .append($addUsername, $addHandle, $addContent, $addTime);
+
+return $result
+}
+
+
+function renderTweets(tweets) {
+  tweets.forEach(function(tweets){
+    $("#tweet-container").append(createTweetElement(tweets));
+  })
+}
+
+renderTweets(data);
+
+})
+
